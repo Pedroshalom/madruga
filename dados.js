c8779c83c10dcbe46161730fe00b810465fa629a
Pedro <pedroshalommrn@hotmail.com>
2023-12-03 07:28:12 -0300

    site madruga

---
+++ dados.js
@@ -0,0 +1,518 @@
+var MENU = {
+    "cachorro-quente": [
+        {
+            "id": "dog1",
+            "img": "./img/cardapio/churrasco/bacon.jpg",
+            "name": "Dog Bacon",
+            "dsc": "Pão, salsicha, bacon, tomate, maionese, ketchup e mostarda.",
+            "price": 5.99
+        },
+        {
+            "id": "dog2",
+            "img": "./img/cardapio/churrasco/dog2.jpg",
+            "name": "Dog Queijo",
+            "dsc": "Pão 20cm, salsicha, muçarela, tomate, maionese, ketchup e mostarda",
+            "price": 5.00
+        },
+        {
+            "id": "dog3",
+            "img": "./img/cardapio/churrasco/dog3.jpg",
+            "name": "Dog Duplo",
+            "dsc": "Pão 20cm, 2 salsichas, tomate, maionese, ketchup e mostarda.",
+            "price": 7.00
+        },
+        {
+            "id": "dog4",
+            "img": "./img/cardapio/churrasco/dog4.jpg",
+            "name": "Dog Calabresa",
+            "dsc": "Pão 20cm, salsicha, calabresa, cebola com shoyu, presunto, muçarela, tomate, maionese, ketchup e mostarda",
+            "price": 7.00
+        },
+        {
+            "id": "dog5",
+            "img": "./img/cardapio/churrasco/dog5.jpg",
+            "name": "Dog American",
+            "dsc": "Pão, hambúrguer, muçarela, alface, picles, tomate, maionese especial, mostarda e molho American Burger.",
+            "price": 6.99
+        },
+        {
+            "id": "dog6",
+            "img": "./img/cardapio/churrasco/dog6.jpg",
+            "name": "Dog Frango",
+            "dsc": "Pão, salsicha, frango desfiado com tempero exclusivo, tomate, maionese especial, ketchup e mostarda.",
+            "price": 5.99
+        },
+        {
+            "id": "dog7",
+            "img": "./img/cardapio/churrasco/dog7.jpg",
+            "name": "Dog Egg",
+            "dsc": "Pão, hambúrguer, presunto, muçarela, ovo, alface, tomate, maionese especial, ketchup e mostarda.",
+            "price": 5.00
+        },
+        {
+            "id": "dog8",
+            "img": "./img/cardapio/churrasco/dog8.jpg",
+            "name": "Dog Simples",
+            "dsc": "Pão 20cm, hambúrguer, muçarela, tomate, cebola, maionese especial, ketchup e mostarda",
+            "price": 5.00
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img": "./img/cardapio/churrasco/the-big-ugly-bbq-dinner-for-6.2dfae7818811adddce85cc1a910881a0.jpg",
+            "name": "Ugly Drum",
+            "dsc": "The Big Ugly BBQ Dinner for 6",
+            "price": 229
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+            "img": "./img/cardapio/churrasco/mighty-quinns-bbq-sampler-pack.1bfe4a0665edc565756f5241bf25840e.jpg",
+            "name": "Mighty Quinn's BBQ",
+            "dsc": "Mighty Quinn's BBQ Sampler Pack",
+            "price": 169
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+            "img": "./img/cardapio/churrasco/post-oak-smoked-usda-prime-half-brisket.6f17178dde7d806670bcc73ff11762b3.jpg",
+            "name": "Southside Market & Barbeque",
+            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
+            "price": 109
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Best of Texas BBQ Combo - Serves 14",
+            "price": 269
+        }
+    ],
+    "burgers": [
+        {
+            "id": "picanha",
+            "img": "./img/cardapio/burguers/b1.avif",
+            "name": "Duplo Picanha",
+            "dsc": "2 hamburgueres de picanha(100g cada), 2 fatias de queijo cheddar, cebola caramelizada, tomate e alface.",
+            "price": 12
+        },
+        {
+            "id": "coalho",
+            "img": "./img/cardapio/burguers/coalho.avif",
+            "name": "Triplo coalho",
+            "dsc": "Pão, 3 Hamburgueres de picanha, 3 Camadas de Queijo Coalho, Barbecue, tomate e alface.",
+            "price": 20
+        },
+        {
+            "id": "classico",
+            "img": "./img/cardapio/burguers/classico.avif",
+            "name": "Clássico",
+            "dsc": "Blend Premium de 150g, Com um Delicioso Queijo Cheddar, Bacon em Tiras, Tomate, Alface, Servido em Pão Artesanal com Gergelim.",
+            "price": 8
+        },
+        {
+            "id": "monstro",
+            "img": "./img/cardapio/burguers/monstro.avif",
+            "name": "Monstro",
+            "dsc": "Blend Premium de 150g, Queijo Cheddar, Anéis de Cebola Empanado, Bacon em Tiras, Tomate e Alface, Servido em Pão Artesanal com Gergelim com Queijo Mussarela Gratinado.",
+            "price": 25
+        },
+        // {
+        //     "id": "shake-shack-shackburger-16-pack",
+        //     "img": "./img/cardapio/burguers/animal5.jpg",
+        //     "name": "Shake Shack",
+        //     "dsc": "Shake Shack Shackburger® - 16 Pack",
+        //     "price": 89
+        // },
+        // {
+        //     "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+        //     "img": "./img/cardapio/burguers/animal6.jpg",
+        //     "name": "Peter Luger Steak House",
+        //     "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+        //     "price": 175.95
+        // },
+        {
+            "id": "duplo",
+            "img": "./img/cardapio/burguers/duplo.avif",
+            "name": "Duplo Cheddar",
+            "dsc": "2 Blend Premium de 150g, 2 Fatias do Delicioso Queijo Cheddar, Barbecue, Tomate, Alface, Servido em Pão Artesanal com Gergelim.",
+            "price": 15
+        },
+        // {
+        //     "id": "goldbelly-burger-bash-pack",
+        //     "img": "./img/cardapio/burguers/animal8.jpg",
+        //     "name": "Pat LaFrieda Meats",
+        //     "dsc": "Goldbelly \"Burger Bash\" Pack",
+        //     "price": 109
+        // },
+        {
+            "id": "bacon",
+            "img": "./img/cardapio/burguers/bacon.avif",
+            "name": "X-Bacon",
+            "dsc": "X-Bacon",
+            "price": 12
+        },
+        {
+            "id": "coalho2",
+            "img": "./img/cardapio/burguers/coalho2.avif",
+            "name": "Duplo Coalho",
+            "dsc": "Duplo Coalho",
+            "price": 17
+        },
+        {
+            "id": "xtudo",
+            "img": "./img/cardapio/burguers/xtudo.jpeg",
+            "name": "X-tudo",
+            "dsc": "X-tudo",
+            "price": 9
+        },
+        {
+            "id": "calabresa",
+            "img": "./img/cardapio/burguers/calabresa.jpg",
+            "name": "X-Calabresa",
+            "dsc": "X-Clabresa",
+            "price": 10
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "petit-gateau",
+            "img": "./img/cardapio/sobremesas/peti.jpg",
+            "name": "Petit Gateau",
+            "dsc": "Petit Gateau",
+            "price": 12
+        },
+        {
+            "id": "Pudim",
+            "img": "./img/cardapio/sobremesas/pudim.jpeg",
+            "name": "Pudim",
+            "dsc": "Pudim",
+            "price": 9
+        },
+        {
+            "id": "mousse",
+            "img": "./img/cardapio/sobremesas/mousse.jpg",
+            "name": "Mousse Chocolate",
+            "dsc": "Mouuse",
+            "price": 5
+        },
+        {
+            "id": "pave",
+            "img": "./img/cardapio/sobremesas/pave.jpg",
+            "name": "Pavê",
+            "dsc": "Pave",
+            "price": 6
+        },
+        {
+            "id": "torta",
+            "img": "./img/cardapio/sobremesas/torta.jpeg",
+            "name": "Torta Limão",
+            "dsc": "Torta de Limão",
+            "price": 7
+        },
+        {
+            "id": "abacaxi",
+            "img": "./img/cardapio/sobremesas/abacaxi.jpeg",
+            "name": "Delícia Abacaxi",
+            "dsc": "abacaxi",
+            "price": 5
+        },
+        {
+            "id": "bolo",
+            "img": "./img/cardapio/sobremesas/bolo.jpeg",
+            "name": "Sensação",
+            "dsc": "Bolo de pote",
+            "price": 8
+        },
+        {
+            "id": "sorvete",
+            "img": "./img/cardapio/sobremesas/sorvete.jpg",
+            "name": "Sorvetes",
+            "dsc": "Jewish Classics Dessert Pack",
+            "price": 6
+        },
+        {
+            "id": "cake",
+            "img": "./img/cardapio/sobremesas/cakke.jpg",
+            "name": "Cake Brigadeiro",
+            "dsc": "Cake",
+            "price": 10
+        },
+        {
+            "id": "frutaã",
+            "img": "./img/cardapio/sobremesas/fruta.jpg",
+            "name": "Fruta",
+            "dsc": "FRuta",
+            "price": 8
+        },
+        {
+            "id": "xcake",
+            "img": "./img/cardapio/sobremesas/xcake.jpg",
+            "name": "Cheesecake",
+            "dsc": "Cheesecake",
+            "price": 11
+        },
+        {
+            "id": "acai",
+            "img": "./img/cardapio/sobremesas/acai.webp",
+            "name": "Açaí",
+            "dsc": "Açaí",
+            "price": 10
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "agua1",
+            "img": "./img/cardapio/bebidas/image.jpg",
+            "name": "Água sem gás",
+            "dsc": "Água sem gás",
+            "price": 4.00
+        },
+        {
+            "id": "agua2",
+            "img": "./img/cardapio/bebidas/image2.jpg",
+            "name": "Água com gás",
+            "dsc": "Água com gás",
+            "price": 5.00
+        },
+        {
+            "id": "refrigerantes",
+            "img": "./img/cardapio/bebidas/image3.jpg",
+            "name": "Refrigerantes",
+            "dsc": "Refrigerantes",
+            "price": 7.00
+        },
+        {
+            "id": "energetico",
+            "img": "./img/cardapio/bebidas/image4.jpg",
+            "name": "Energético",
+            "dsc": "Energético",
+            "price": 10.00
+        },
+        {
+            "id": "suco1",
+            "img": "./img/cardapio/bebidas/image5.jpg",
+            "name": "Suco de Uva",
+            "dsc": "Suco de Uva",
+            "price": 9
+        },
+        {
+            "id": "suco2",
+            "img": "./img/cardapio/bebidas/image6.jpg",
+            "name": "Suco de Limão",
+            "dsc": "Suco de Limão",
+            "price": 9
+        },
+        {
+            "id": "suco3",
+            "img": "./img/cardapio/bebidas/image7.jpg",
+            "name": "Suco de Laranja",
+            "dsc": "Suco de Laranja",
+            "price": 9
+        },
+        {
+            "id": "suco4",
+            "img": "./img/cardapio/bebidas/image8.jpg",
+            "name": "Suco de Abacaxi com Hortelã",
+            "dsc": "Suco de Abacaxi com Hortelã",
+            "price": 9
+        },
+        {
+            "id": "banana",
+            "img": "./img/cardapio/bebidas/v1.avif",
+            "name": "Vitamina de  Banana",
+            "dsc": "Vitamina de Banana",
+            "price": 9
+        },
+        {
+            "id": "abacate",
+            "img": "./img/cardapio/bebidas/abacate.jpeg",
+            "name": "Vitamina de Abacate",
+            "dsc": "Vitamina de Abacate",
+            "price": 9
+        },
+        {
+            "id": "aazonas",
+            "img": "./img/cardapio/bebidas/am.png",
+            "name": "Guaraná do Amazonas",
+            "dsc": "Guaraná do Amazonas",
+            "price": 9
+        },
+        {
+            "id": "milk",
+            "img": "./img/cardapio/bebidas/milk.jpg",
+            "name": "Milk-Sakes",
+            "dsc": "Milk-Shakes",
+            "price": 10
+        }
+    ],
+    "salgados": [
+        {
+            "id": "coxinha",
+            "img": "./img/cardapio/pizzas/coxa.jpg",
+            "name": "Coxinha de frago",
+            "dsc": "Coxinha",
+            "price": 3.00
+        },
+        {
+            "id": "pastel",
+            "img": "./img/cardapio/pizzas/ps.jpg",
+            "name": "Pastel",
+            "dsc": "Pastel",
+            "price": 3.00
+        },
+        {
+            "id": "pastell",
+            "img": "./img/cardapio/pizzas/pastel.jpeg",
+            "name": "Pastel de Forno",
+            "dsc": "Pastel",
+            "price": 3.00
+        },
+        {
+            "id": "queijada",
+            "img": "./img/cardapio/pizzas/que.jpeg",
+            "name": "Queijada",
+            "dsc": "Queijada",
+            "price": 3.00
+        },
+        {
+            "id": "enroladinho",
+            "img": "./img/cardapio/pizzas/enrri.jpg",
+            "name": "Enroladinho",
+            "dsc": "Enroladinho",
+            "price": 3.00
+        },
+        {
+            "id": "kibe",
+            "img": "./img/cardapio/pizzas/kk.jpeg",
+            "name": "Kibe",
+            "dsc": "KIbe",
+            "price": 5.00
+        },
+        {
+            "id": "empada",
+            "img": "./img/cardapio/pizzas/empp.jpg",
+            "name": "Empada",
+            "dsc": "Empada",
+            "price": 3.00
+        },
+        {
+            "id": "risoli",
+            "img": "./img/cardapio/pizzas/risole.jpg",
+            "name": "Risole",
+            "dsc": "Risole",
+            "price": 3.00
+        },
+        {
+            "id": "croassanit",
+            "img": "./img/cardapio/pizzas/croassanit.jpg",
+            "name": "Croassant",
+            "dsc": "Croassant",
+            "price": 3.50
+        },
+        {
+            "id": "mini",
+            "img": "./img/cardapio/pizzas/mini.webp",
+            "name": "Mini Pizza",
+            "dsc": "Mini Pizza",
+            "price": 5.00
+        },
+        {
+            "id": "foleado",
+            "img": "./img/cardapio/pizzas/folleado.jpeg",
+            "name": "Folleado",
+            "dsc": "Folleado",
+            "price": 3.00
+        },
+        {
+            "id": "empadas",
+            "img": "./img/cardapio/pizzas/empadas.jpeg",
+            "name": "Empada Doce",
+            "dsc": "Empada Doce",
+            "price": 3.00
+        }
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steaks-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/valentines-steaks-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
+            "name": "Chesapeake Bay Gourmet",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 129
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
+            "name": "Saltbrick Prime",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 179
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/steaks/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
+            "name": "Old Homestead Steakhouse",
+            "dsc": "Bone-in Rib Steak",
+            "price": 159
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/steaks/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
+            "name": "Snake River Farms",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 119
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/steaks/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
+            "name": "Peter Luger Steak House",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 215.95
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
+            "name": "Churchill's Steakhouse",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 229
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/steaks/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 96.7
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/cardapio/steaks/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 32
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+            "img": "./img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
+            "name": "Chef Francis Mallmann",
+            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
+            "price": 225
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steak.ff15071964ec8141d30c2ba05fb117e0.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steak",
+            "price": 22
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+            "img": "./img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
+            "name": "Perini Ranch Steakhouse",
+            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
+            "price": 165
+        }
+    ]
+}
